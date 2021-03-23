c4aed64d0874704c2408375e4ec547261cd9f18b
farhansam <farhan_samsol@hotmail.com>
2021-03-23 14:10:49 +0000

    created results page

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
+import pymongo
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
+MONGO_URI = os.environ.get('MONGO_URI')
+DB_NAME = 'sample_restaurants'
+
+client = pymongo.MongoClient(MONGO_URI)
+db = client[DB_NAME]
 
 @app.route('/')
 def home():
     return render_template('home.template.html')
+
+@app.route('/results')
+def show_results():
+    return render_template('results.template.html')
 
 
 # "magic code" -- boilerplate
