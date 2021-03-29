3750c77dd7439a282662fc268cf47f8b2a5b36b0
farhansam <farhan_samsol@hotmail.com>
2021-03-29 07:44:27 +0000

    created route for deleting

--- app.py
+++ app.py
@@ -15,13 +15,25 @@ DB_NAME = 'thr_db'
 client = pymongo.MongoClient(MONGO_URI)
 db = client[DB_NAME]
 
+
+
+
+
+
 # Home page
 @app.route('/')
 def home():
     return render_template('home.template.html')
 
 
-# Create info page
+
+
+
+
+
+
+
+# Create stall
 @app.route('/stall/create')
 def show_create_stall():
     return render_template('create_stall.template.html')
@@ -49,11 +61,20 @@ def process_create_stall():
         "specialty_dish_2": specialty_dish_2,
         "specialty_dish_3": specialty_dish_3
     })
-    
+
     return redirect(url_for('show_create_stall'))
 
 
-# Results page
+
+
+
+
+
+
+
+
+
+# Show all stalls & search for stall
 @app.route('/stall/results')
 def show_search_results():
     stall_name_search = request.args.get('stall_name_search')
@@ -71,10 +92,46 @@ def show_search_results():
     })
     return render_template('results.template.html', all_stalls=all_stalls)
 
+
+
+
+
+
+
+# Delete stall
+@app.route('/stall/<stall_id>/delete')
+def delete_stall(stall_id):
+    stall = db.foodStalls.find_one({
+        '_id': ObjectId(stall_id)
+    })
+
+    return render_template('confirm_delete_stall.template.html',
+                           stall_to_delete=stall)
+
+
+@app.route('/stall/<stall_id>/delete', methods=["POST"])
+def process_delete_stall(stall_id):
+    db.foodStalls.remove({
+        "_id": ObjectId(stall_id)
+    })
+    return redirect(url_for('show_search_results'))
+
+
+
+
+
+
+
 # Display page
 @app.route('/stall/display')
 def show_stall_info():
     return render_template('display.template.html')
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
