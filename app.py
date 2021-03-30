2a4b3419e2ee7d0ff2638114426af676eb0a6918
farhansam <farhan_samsol@hotmail.com>
2021-03-30 12:58:57 +0000

    filter function tested and working

--- app.py
+++ app.py
@@ -16,27 +16,17 @@ client = pymongo.MongoClient(MONGO_URI)
 db = client[DB_NAME]
 
 
-
-
-
-
 # Home page
 @app.route('/')
 def home():
     return render_template('home.template.html')
 
 
-
-
-
-
-
-
-
 # Create stall
 @app.route('/stall/create')
 def show_create_stall():
     return render_template('create_stall.template.html')
+
 
 @app.route('/stall/create', methods=["POST"])
 def process_create_stall():
@@ -65,36 +55,57 @@ def process_create_stall():
     return redirect(url_for('show_create_stall'))
 
 
-
-
-
-
-
-
-
-
-
 # Show all stalls & search for stall
 @app.route('/stall/results')
 def show_search_results():
-    stall_name_search = str(request.args.get('stall_name_search'))
+    find_stall_name = str(request.args.get('find_stall_name'))
+    find_area = request.args.get('find_area')
+    find_estate = request.args.get('find_estate')
+    find_cuisine = request.args.get('find_cuisine')
+    find_grading = request.args.get('find_grading')
+    find_rating = request.args.get('find_rating')
+    find_review = request.args.get('find_review')
 
-    all_stalls = db.foodStalls.find({
-        'stall_name': {
-            '$regex': stall_name_search, '$options': 'i'
-        }
-    }, {
+    # if find_stall_name:
+    #     find_stall = db.foodStalls.find({
+    #         'stall_name': {
+    #             '$regex': find_stall_name, '$options': 'i'
+    #         }
+    #     }, {
+    #         'stall_name': 1,
+    #         'area': 1,
+    #         'grading': 1,
+    #         'address': 1
+    #     })
+    # return render_template('results.template.html', find_stall=find_stall)
+
+    criteria = {}
+
+    if find_area:
+        criteria['area'] = find_area
+    
+    if find_estate:
+        criteria['estate'] = find_estate
+
+    if find_cuisine:
+        criteria['cuisine'] = find_cuisine
+    
+    if find_grading:
+        criteria['grading'] = find_grading
+
+    if find_rating:
+        criteria['rating'] = find_rating
+
+    if find_review:
+        criteria['review'] = find_review
+
+    listings = db.foodStalls.find(criteria, {
         'stall_name': 1,
         'area': 1,
         'grading': 1,
         'address': 1
-    })
-    return render_template('results.template.html', all_stalls=all_stalls)
-
-
-
-
-
+    }).limit(20)
+    return render_template('results.template.html', listings=listings)
 
 
 # Delete stall
@@ -116,21 +127,10 @@ def process_delete_stall(stall_id):
     return redirect(url_for('show_search_results'))
 
 
-
-
-
-
-
 # Display page
 @app.route('/stall/display')
 def show_stall_info():
     return render_template('display.template.html')
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
