6cec6eadd4699807d8c9d3a6df2c7b4db9be5b9e
farhansam <farhan_samsol@hotmail.com>
2021-04-10 11:28:05 +0000

    edited create review form

--- app.py
+++ app.py
@@ -60,7 +60,8 @@ def show_stalls_in_hawker(hawker_centre):
 # Show form to create stall
 @app.route('/stall/create')
 def show_create_stall():
-    return render_template('create_stall.template.html')
+    all_hawker = db.hawkerCentres.find()
+    return render_template('create_stall.template.html', all_hawker=all_hawker)
 
 # Process form to create stall
 @app.route('/stall/create', methods=["POST"])
@@ -106,9 +107,25 @@ def show_all_stalls():
 
 
 # Display stall information and show form to create review
-@app.route('/stall/<stall_id>/<stall_name>/<address>/display')
-def show_stall_info(stall_id, stall_name, address):
+@app.route('/stall/<stall_id>/<stall_name>/<hawker_centre>/display')
+def show_stall_info(stall_id, stall_name, hawker_centre):
     reviewed_stall_id = ObjectId(stall_id)
+
+    stall_to_display = db.foodStalls.find({
+        '_id': reviewed_stall_id
+    }, {
+        'stall_name':1,
+        'hawker_centre':1,
+        'specialty':1,
+        'unit_no':1,
+        'opening_hours':1
+    })
+
+    hawker_to_display = db.hawkerCentres.find({
+        'name': hawker_centre
+    }, {
+        'address':1
+    })
 
     stall_reviews = db.stallReviews.find({
         'reviewed_stall_id': reviewed_stall_id
@@ -117,33 +134,35 @@ def show_stall_info(stall_id, stall_name, address):
         'comment':1
     })
 
-    return render_template('display.template.html', stall_id=stall_id, stall_name=stall_name, address=address, stall_reviews=stall_reviews)
+    return render_template('display.template.html', stall_id=stall_id, stall_name=stall_name, stall_reviews=stall_reviews, stall_to_display=stall_to_display, hawker_to_display=hawker_to_display)
 
 # Process form to create review
-@app.route('/stall/<stall_id>/<stall_name>/<address>/display', methods=["POST"])
-def process_create_review(stall_id, stall_name, address):
+@app.route('/stall/<stall_id>/<stall_name>/<hawker_centre>/display', methods=["POST"])
+def process_create_review(stall_id, stall_name, hawker_centre):
     user_name = request.form.get('user_name')
     comment = request.form.get('comment')
     reviewed_stall_name = stall_name
     reviewed_stall_id = ObjectId(stall_id)
 
     db.stallReviews.insert_one({
-        "user_name": user_name.lower(),
-        "comment": comment.lower(),
-        "reviewed_stall_name": stall_name.lower(),
+        "user_name": user_name,
+        "comment": comment,
+        "reviewed_stall_name": stall_name,
         "reviewed_stall_id": reviewed_stall_id
     })
 
-    return redirect(url_for('show_stall_info', stall_id=stall_id, stall_name=stall_name, address=address))
+    return redirect(url_for('show_stall_info', stall_id=stall_id, stall_name=stall_name, hawker_centre=hawker_centre))
 
 
 # Update stall
 @app.route('/stall/<stall_id>/update')
 def show_update_stall(stall_id):
+    all_hawker = db.hawkerCentres.find()
+
     stall = db.foodStalls.find_one({
         '_id': ObjectId(stall_id)
     })
-    return render_template('show_update_stall.template.html',
+    return render_template('show_update_stall.template.html', all_hawker=all_hawker,
                            stall=stall)
 
 # Process to update stall
